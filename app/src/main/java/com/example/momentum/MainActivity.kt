40d8728349e106ed6adfd52fa94f7a7f984fdf6a
KavinMadusanka <kavinmadusanka20011@gmail.com>
2024-05-11 23:20:12 +0530

    add note interface

--- app/src/main/java/com/example/momentum/MainActivity.kt
+++ app/src/main/java/com/example/momentum/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.momentum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.momentum.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent (this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
